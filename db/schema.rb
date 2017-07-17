15e5bf4969bec7c9d5e46c680a5d9c49cc37fcd1
zpalmer <zoe.palmer@getbraintree.com>
2017-07-17 10:55:15 -0500

    fixup! Add votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717153340) do
+ActiveRecord::Schema.define(version: 20170717999999) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "genre"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
