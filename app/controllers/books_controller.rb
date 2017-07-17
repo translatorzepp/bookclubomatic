9dfd516817fd61063a01ae36dfc83f40672c8de7
zpalmer <zoe.palmer@getbraintree.com>
2017-07-17 12:04:45 -0500

    Add vote method to book and route for voting

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -61,6 +61,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def vote
+    @book = Book.find(params[:id]) #searches db for Book with specified ID
+    @book.vote.create
+    redirect_to(book_path) #book_path is the book list
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
