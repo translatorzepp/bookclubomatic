83d8e55d5c3a18932e9b8542bcd93e3a9affd5d7
zpalmer <zoe.palmer@getbraintree.com>
2017-08-17 09:33:53 -0500

    Add redirect test

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, :type => :controller do
-  # render_views
-  # subject { Book.new }
+  render_views
+
+  context "it only allows whitelisted params" do
+    it "requires a book" do
+    end
+
+    it "permits a voter name" do
+    end
+
+    it "does not permit other params" do
+    end
+  end
 
   context "book" do
     it "creates a book" do
@@ -11,12 +21,36 @@ RSpec.describe BooksController, :type => :controller do
       expect(book.title).to eq("Ancillary Justice")
     end
 
+    it "redirects to the index page after creation" do
+      post :create, :params => {:book => {:title => "Ancillary Justice"}}
+      expect(response.get_header("Location").end_with?("/books")).to be true
+    end
+
     it "does not create a book without a title" do
       expect{ post :create, :params => {:book => {}} }.to raise_error(ActionController::ParameterMissing)
+    end
+
+    it "deletes a book" do
+      # post :create, :params => {:book => {:title => "Ancillary Justice"}}
+      # book = Book.find_by(:title => "Ancillary Justice")
+      # expect{ delete :destroy, :params => {:book => {:id  => book.id}} }.to change{ Book.all.count }.by(-1)
+      # expect(response.get_header("Location").end_with?("/books")).to be true
     end
   end
 
   context "votes" do
+    it "votes for a book" do
+    end
 
+    it "does not vote for a book without a voter_name" do
+    end
+
+    it "unvotes for a book" do
+    end
+  end
+
+  context "book with votes" do
+    it "does not delete a book with votes" do
+    end
   end
 end
